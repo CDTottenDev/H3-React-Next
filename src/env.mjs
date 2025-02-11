6c43174efdc01467908dc968536f703f2fd90473
CDTottenDev <cdtottendev@gmail.com>
2025-02-11 12:21:53 -0800

    feat: prisma cleanup

--- src/env.mjs
+++ src/env.mjs
@@ -3,8 +3,6 @@ import { z } from 'zod';
 
 export const env = createEnv({
   server: {
-    DATABASE_URL: z.string().url(),
-    DIRECT_URL: z.string().url(),
     RESEND_API_KEY: z.string().min(1),
     APP_URL: z.string().url(),
     GOOGLE_SITE_VERIFICATION_ID: z.string().optional(),
@@ -14,8 +12,6 @@ export const env = createEnv({
     NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(1),
   },
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
-    DIRECT_URL: process.env.DIRECT_URL,
     RESEND_API_KEY: process.env.RESEND_API_KEY,
     APP_URL: process.env.APP_URL,
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
